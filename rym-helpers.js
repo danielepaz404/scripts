c6ff05686bff454296f21a867d9974dd69541b0f
Daniele Paz <79173163+danielepaz404@users.noreply.github.com>
2025-09-06 00:27:13 -0300

    :sparkles: add rym helper

---
+++ rym-helpers.js
@@ -0,0 +1,8 @@
+function getTrackRatingSum() {
+    total = 0;
+    document.querySelectorAll("[id*=rating_num_z_]").forEach(val => {
+        const content = val.textContent;
+        if (isNaN(content)) return;
+        total += Number(val.textContent)
+    })
+}
