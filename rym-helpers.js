b832bc9c1eb365efdea37286061951f97c7795ff
Daniele Paz <79173163+danielepaz404@users.noreply.github.com>
2025-09-06 00:49:13 -0300

    :art: update param name

--- rym-helpers.js
+++ rym-helpers.js
@@ -2,9 +2,10 @@ function getTrackRatingSum() {
     total = 0;
     totalTrackCount = track_ratings?.ratings?.length;
 
-    track_ratings?.ratings?.forEach(rating => {
-        total += rating.rating;
+    track_ratings?.ratings?.forEach(track => {
+        total += track.rating;
     });
 
     return totalTrackCount > 0 ? (total / totalTrackCount).toFixed(2) : 0;
 }
+
