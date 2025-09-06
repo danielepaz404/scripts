0fd4c511ea22e033ebddc57fa070b260bcb31dd7
Daniele Paz <79173163+danielepaz404@users.noreply.github.com>
2025-09-06 00:34:14 -0300

    :sparkles: add count and return

--- rym-helpers.js
+++ rym-helpers.js
@@ -1,8 +1,14 @@
 function getTrackRatingSum() {
     total = 0;
-    document.querySelectorAll("[id*=rating_num_z_]").forEach(val => {
+    trackCount = 0;
+    const trackEls = document.querySelectorAll("[id*=rating_num_z_]");
+    
+    trackEls.forEach(val => {
         const content = val.textContent;
         if (isNaN(content)) return;
         total += Number(content);
+        trackCount++;
     });
+
+    return (total/trackCount).toFixed(2);
 }
